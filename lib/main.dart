3bd7bb8b55fecc9eeca0bd1d9248e2f7db17fea2
yuuna tatsunaka <jdazhong966@gmail.com>
2023-06-19 14:04:51 +0900

    gitについて少しわかってきた、かも...?

---
+++ lib/main.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            '埴輪',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 16,
+            ),
+          ),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              TweetTile(),
+              TweetTile(),
+              TweetTile(),
+              TweetTile(),
+              TweetTile(),
+              TweetTile(),
+              TweetTile(),
+              TweetTile(),
+              TweetTile(),
+              TweetTile(),
+              TweetTile(),
+              TweetTile(),
+              TweetTile(),
+              TweetTile(),
+              TweetTile(),
+              TweetTile(),
+              TweetTile(),
+              TweetTile(),
+              TweetTile(),
+              TweetTile(),
+              TweetTile(),
+              TweetTile(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class TweetTile extends StatelessWidget {
+  const TweetTile({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(8.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const CircleAvatar(
+            backgroundImage: NetworkImage(
+                'https://qualia-45.jp/wp-content/uploads/haniwa_p6.jpg'),
+          ),
+          SizedBox(width: 8),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  Text('前方後円墳'),
+                  SizedBox(width: 8),
+                  Text('2023/6/13'),
+                ],
+              ),
+              SizedBox(height: 4),
+              Text('埴輪のミニチャームかわいい!'),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.favorite_border),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
